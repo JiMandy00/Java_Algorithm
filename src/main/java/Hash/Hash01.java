11c7d22d6af8a1c1c612aaf6bdaae10a16b678c5
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-10-26 11:37:40 +0900

    2022 10 25
    Hash를 생성해서 데이터를 삽입
    (단, 중복은 신경쓰지 않는다)

---
+++ src/main/java/Hash/Hash01.java
@@ -0,0 +1,20 @@
+package Hash;
+
+import java.sql.SQLOutput;
+
+public class Hash01 {
+    public int hash(String str) {
+        int ascii = 0;
+        for (int i = 0; i < str.length(); i++) {
+            ascii += str.charAt(i);
+
+        }
+        return ascii % 1000; // 1000개
+    }
+
+    public static void main(String[] args) {
+        Hash01 h = new Hash01();
+
+        System.out.println(h.hash("안녕"));
+    }
+}
