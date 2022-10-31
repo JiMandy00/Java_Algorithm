6f6e47080e97eda6c0152a4da85ded6430bf5d11
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-10-31 09:43:35 +0900

    2022 10 31 - 프로그래머스 모의고사 (문제 분석)

---
+++ src/main/java/prepare_test/PreparedTest.java
@@ -0,0 +1,36 @@
+// 22 10 31
+// 프로그래머스 모의고사
+
+// 1번 수포자 1, 2, 3, 4, 5
+// 2번 수포자 2, 1, 2, 3, 2, 4, 2, 5
+// 3번 수포자 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
+
+// answers [1, 2, 3, 4, 5] [1]
+// answers [1, 3, 2, 4, 2] [1, 2, 3]
+
+package prepare_test;
+
+public class PreparedTest {
+
+    int member = 3;
+
+    int[] supoja_1 = {1, 2, 3, 4, 5};
+    int[] supoja_2 = {2, 1, 2, 3, 2};
+    int[] supoja_3 = {3, 3, 1, 1, 2};
+
+    public int[] solution(int[] answers) {
+
+        for (int i = 1; i <= member; i++) {
+            for (int j = 0; j < answers.length; j++) {
+                if ()
+            }
+        }
+
+        return answers;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
