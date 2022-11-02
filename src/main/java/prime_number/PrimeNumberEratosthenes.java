c97cd60b46605a51595f2addba5b8db5594f9362
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-02 09:37:49 +0900

    2022 11 02 - 프로그래머스 소수찾기(2) (구현 : 2 ~ 50을 리스트에 담고 2의 배수를 지우기)

---
+++ src/main/java/prime_number/PrimeNumberEratosthenes.java
@@ -0,0 +1,38 @@
+// 22 11 02
+// 프로그래머스 - 소수 찾기
+// 에라토스테네스의 체를 적용
+
+// 알고리즘
+// N x N = 50 미만의 모든 소수를 구하는 에라토스테네스의 체
+// 1. 2부터 50까지 숫자가 들어있는 LIst
+//  1.1. 잘 지웠는지 확인해보자
+// 2. 만든 List에서 2를 제외한 모든 2의 배수 지우기
+// 3. 3을 제외한 모든 3의 배수 지우기 ... 7까지 반복. 왜냐하면 List는 50까지가 전부고 7의 배수가 49로 List에서 제일 큰 값의 배수를 가졌기 때문에
+
+
+package prime_number;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeNumberEratosthenes {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int howManyRe = 50;
+        for (int i = 2; i < howManyRe; i++) {
+            arrayList.add(i);
+        }
+
+        for (int i = 2; i < arrayList.size(); i++) {
+            if (2*i > 50) {
+                break;
+            }
+            arrayList.remove(new Integer(2*i));
+        }
+
+        System.out.println(arrayList);
+
+
+    }
+}
