754928e8c7b2ed4e0ef2fbf363475f0ecd8d47a5
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-03 09:28:53 +0900

    2022 11 03 - 프로그래머스 소수찾기(3) (알고리즘 : List를 Array로 변경해서 다시 풀이)

---
+++ src/main/java/prime_number/PrimeNumberEratosthenes02.java
@@ -0,0 +1,25 @@
+// 22 11 02
+// 프로그래머스 - 소수 찾기
+// 에라토스테네스의 체를 적용
+
+// 알고리즘
+// N x N = 50 미만의 모든 소수를 구하는 에라토스테네스의 체
+// 1. 2부터 50까지 숫자가 들어있는 Array
+// 2. num 행, check행을 만들어서 해당 수가 소수인 본인이 아니며 자신의 배수이면 check에 false를 삽입. 아니면 true
+
+
+package prime_number;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeNumberEratosthenes02 {
+    public int solution(int n) { // 1 ~ n 사이의 정수
+        int anwser = 0;
+        return anwser;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
