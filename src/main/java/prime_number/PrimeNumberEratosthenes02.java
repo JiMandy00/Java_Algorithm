8dae10f345050dd53ee9180d9849272c59ef9af9
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-03 10:14:00 +0900

    2022 11 03 - 프로그래머스 소수찾기(3) (구현 : 기본 세팅)

--- src/main/java/prime_number/PrimeNumberEratosthenes02.java
+++ src/main/java/prime_number/PrimeNumberEratosthenes02.java
@@ -11,15 +11,31 @@
 package prime_number;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PrimeNumberEratosthenes02 {
-    public int solution(int n) { // 1 ~ n 사이의 정수
-        int anwser = 0;
-        return anwser;
+    public void solution(int n) { // 1 ~ n 사이의 정수
+
+        int N = 50; // N = 50
+
+        int[] nums = new int[N-1]; // N-1개만큼 2~50까지 총 49개
+
+        boolean[] checks = new boolean[nums.length]; // nums만큼 checks생성
+
+        Arrays.fill(checks, true); // checks를 true로 초기화 cuz 기본값 false
+
+        for (int i = 0; i < nums.length; i++) nums[i] = i + 2; // 2~50까지 nums에 채우기
+
+
+
     }
 
     public static void main(String[] args) {
+        PrimeNumberEratosthenes pne = new PrimeNumberEratosthenes();
+        pne.solution(10);
+
+
 
     }
 }
