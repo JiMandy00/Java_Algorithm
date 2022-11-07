de2cb0d90763be323cf11b575ab169df53cce6a9
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-07 09:33:34 +0900

    2022 11 07 - 프로그래머스 하샤드 수 (구현 : 하샤드 수 구하기)

--- src/main/java/test01.java
+++ src/main/java/test01.java
@@ -2,6 +2,7 @@
 // 하샤드 수
 // 입력받은 수를 분리해서 배열에 담기
 // array를 이용하지 않아도 더해서 사용 가능할 거 같아서.. array를 없앴다.
+
 import java.util.Arrays;
 
 public class test01 {
@@ -14,7 +15,11 @@ public class test01 {
             num = num / 10;
         }
 
-        System.out.println(sum);
+        if (num%sum == 0) {
+            System.out.println("하샤드 수가 맞습니다.");
+        } else {
+            System.out.println("하샤드 수가 아닙니다.");
+        }
 
 
     }
