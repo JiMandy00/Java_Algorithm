4778dab04f2ff3a7a62089f4022a0d683cb75209
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-07 09:31:54 +0900

    2022 11 07 - 프로그래머스 하샤드 수 (구현 : 배열 없애기)

--- src/main/java/test01.java
+++ src/main/java/test01.java
@@ -1,21 +1,20 @@
 // 22 11 07
 // 하샤드 수
 // 입력받은 수를 분리해서 배열에 담기
+// array를 이용하지 않아도 더해서 사용 가능할 거 같아서.. array를 없앴다.
 import java.util.Arrays;
 
 public class test01 {
     public static void main(String[] args) {
-        int[] splitNum = new int[4];
         int num = 621;
 
-        int arrNumber = 0;
+        int sum = 0;
         while (num != 0) {
-            splitNum[arrNumber] = num % 10;
-            arrNumber++;
+            sum += num % 10;
             num = num / 10;
         }
 
-        System.out.println(Arrays.toString(splitNum));
+        System.out.println(sum);
 
 
     }
