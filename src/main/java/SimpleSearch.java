07bf5a39d4c951cf9d9cdb385a472f9568b07329
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-08 09:48:35 +0900

    2022 11 08 - 코드업 (Simple search로 풀이)

---
+++ src/main/java/SimpleSearch.java
@@ -0,0 +1,29 @@
+// 22 11 08
+// simple search
+
+// [question]
+// k를 찾으면 위치를, 찾지 못하면 -1 출력
+
+// [data]
+// 10
+// 2 3 7 4 9 10 223 111 23 39
+// 111
+
+
+
+
+import java.util.Scanner;
+
+public class SimpleSearch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+    }
+}
--- src/main/java/test02.java
+++ src/main/java/test02.java
@@ -1,30 +1,40 @@
-import java.util.Arrays;
-
-// 22 11 07
-// 나누어 떨어지는 숫자 배열
-public class test02 {
-    class Solution {
-        public int[] solution(int[] arr, int divisor) {
-            int[] answer = arr;
-            int[] answerTest = {};
-            int cnt = 0;
-
-            for (int i = 0; i < answer.length; i++) {
-                if (answer[i] % divisor == 0) {
-                    answerTest[cnt] = answer[i];
-                }
-            }
-
-            answer = answerTest;
-
-            return answer;
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {5, 9, 7, 10};
-        int divisor = 5;
-        Solution solution = new Solution(arr, divisor);
-        solution.solution(arr, divisor);
-    }
-}
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//// 22 11 07
+//// 나누어 떨어지는 숫자 배열
+//// 배열에서 계산해서 list에 넣고
+//// list에서 다시 array로 변경
+//// array를 정렬해서 return
+//
+//public class test02 {
+//    class Solution {
+//        public int[] solution(int[] arr, int divisor) {
+//            List<Integer> list = new ArrayList<>();
+//            for (int i = 0; i < arr.length; i++) {
+//                if (arr[i]%divisor == 0) {
+//                    list.add(arr[i]);
+//                }
+//            }
+//
+//            if (list.size() == 0) {
+//                return new int[-1];
+//            }
+//
+//            int[] answer = new int[list.size()];
+//
+//            for (int i = 0; i < list.size(); i++) {
+//                answer[i] = list.get(i);
+//            }
+//
+//            Arrays.sort(arr);
+//            return answer;
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Solution solution = new Solution();
+//        solution.solution();
+//    }
+//}
