cb3db8d4387b9101a6d61621053d55ffe713e601
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-09 10:16:48 +0900

    2022 11 09 - Selection Sort (구현 : 오름차순, 내림차순 완료)

--- src/main/java/selection_sort/SelectionSort.java
+++ src/main/java/selection_sort/SelectionSort.java
@@ -3,7 +3,8 @@
 
 // 1. 최대값이 들어있는 인덱스 찾기
 // 2. 최소값이 들어있는 인덱스 찾기
-// 3. 선택정렬 구현
+// 3. 선택정렬 내림차순 구현
+// 4. 선택정렬 오름차순 구현
 
 package selection_sort;
 
@@ -14,19 +15,39 @@ public class SelectionSort {
 
         int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
+        // 오름차순
         int minIdx = 0;
-        int tmp = 0;
+        int tmp1 = 0;
 
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length-1; i++) { // 0부터 순차적으로 배열에 담는 로직
             minIdx = i;
             for (int j = i+1; j < arr.length; j++) {
                 if (arr[j] < arr[minIdx]) {
                     minIdx = j;
                 }
             }
-            tmp = arr[minIdx];
+            tmp1 = arr[minIdx];
             arr[minIdx] = arr[i];
-            arr[i] = tmp;
+            arr[i] = tmp1;
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        // 내림차순
+        int maxIdx = 0;
+        int tmp2 = 0;
+
+        for (int i = 0; i < arr.length-1; i++) { // 0부터 순차적으로 배열에 담는 로직
+            minIdx = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] > arr[maxIdx]) {
+                    maxIdx = j;
+                }
+            }
+            tmp2 = arr[maxIdx];
+            arr[maxIdx] = arr[i];
+            arr[i] = tmp2;
 
         }
 
