eff9ae5a3474eb80b4a781c3731d30ec722a814f
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-09 10:12:58 +0900

    2022 11 09 - Selection Sort (구현 완료)

--- src/main/java/selection_sort/SelectionSort.java
+++ src/main/java/selection_sort/SelectionSort.java
@@ -3,6 +3,7 @@
 
 // 1. 최대값이 들어있는 인덱스 찾기
 // 2. 최소값이 들어있는 인덱스 찾기
+// 3. 선택정렬 구현
 
 package selection_sort;
 
@@ -10,31 +11,27 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
+
         int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
-        int max = arr[arr.length-1];
-        int maxIdx = arr.length;
-        int min = 0;
         int minIdx = 0;
         int tmp = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            min = arr[i];
-            if (min > max) {
-                tmp = max;
-                max = min;
-                min = tmp;
-                maxIdx = i;
+        for (int i = 0; i < arr.length-1; i++) {
+            minIdx = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
             }
+            tmp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = tmp;
 
-            if (min < max) {
-                minIdx = i;
-            }
         }
-        System.out.println("min : " + min);
-        System.out.println("minIdx : " + minIdx);
-        System.out.println("max : " + max);
-        System.out.println("maxIdx : " + maxIdx);
 
+        System.out.println(Arrays.toString(arr));
     }
+
 }
+
