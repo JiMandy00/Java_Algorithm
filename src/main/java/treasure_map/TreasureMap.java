a7fbbab614e0d6dcb26f2ca4b994531adbc15e71
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-11 10:36:08 +0900

    2022 11 11 - 프로그래머스_비밀 지도(구현 : 2진수로 변환시 앞자리 0이 나오지 않는 문제 해결)

--- src/main/java/treasure_map/TreasureMap.java
+++ src/main/java/treasure_map/TreasureMap.java
@@ -9,20 +9,25 @@ public class TreasureMap {
 
         String[] arr1B = new String[arr1.length];
         String[] arr2B = new String[arr2.length];
+        String[][] arr1M = new String[n][n];
+        String[][] arr2M = new String[n][n];
 
+        // 10진수 -> 2진수
         for (int i = 0; i < n; i++) {
-            arr1B[i] = Integer.toBinaryString(arr1[i]);
-            arr2B[i] = Integer.toBinaryString(arr2[i]);
+            arr1B[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr1[i])));
+            arr2B[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr2[i])));
         }
         System.out.println(Arrays.toString(arr1B));
         System.out.println(Arrays.toString(arr2B));
 
+
         return "";
     }
+
     public static void main(String[] args) {
         TreasureMap treasureMap = new TreasureMap();
-        int[] arr1Ex = {9,20,28,18,11};
-        int[] arr2Ex = {30,1,21,17,28};
+        int[] arr1Ex = {9, 20, 28, 18, 11};
+        int[] arr2Ex = {30, 1, 21, 17, 28};
         treasureMap.solution(5, arr1Ex, arr2Ex);
     }
 }
