b3e3d73eb97ca13d2df42023230b24ef9ced9944
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-15 10:10:11 +0900

    2022 11 15 - Quick sort (이전 코드 sort 메서드로 분리)

--- src/main/java/quick_sort/QuickSort.java
+++ src/main/java/quick_sort/QuickSort.java
@@ -5,28 +5,38 @@ import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort {
-    public static void main(String[] args) {
 
-
-        int[] arr  = { 20, 18, 5, 19, 5, 25, 40, 50};
-
+    public List<Integer> sort(List<Integer> arr) {
         // 1. 기준값 뽑기
-        int pivot = arr[arr.length/2];
+        int pivot = arr.get(arr.size()/2);
         System.out.println(pivot);
 
         // 2. 기준값 기준으로 왼/오 나누어 담기
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            if(pivot > arr[i]) {
-                left.add(arr[i]);
-            } else if (pivot < arr[i]) {
-                right.add(arr[i]);
+        for (int i = 0; i < arr.size(); i++) {
+            if(pivot > arr.get(i)) {
+                left.add(arr.get(i));
+            } else if (pivot < arr.get(i)) {
+                right.add(arr.get(i));
             }
         }
         System.out.println(left);
         System.out.println(right);
+
+        return sort(left) + pivot + sort(right);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr  = { 20, 18, 5, 19, 5, 25, 40, 50};
+        List<Integer> arrList = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            arrList.add(arr[i]);
+        }
+
+
 
 
 
