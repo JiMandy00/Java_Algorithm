595f3f459a300c95c887d4748439da5defdcc2d5
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-22 09:42:09 +0900

    2022 11 22 - 재귀함수(탈출 조건 추가)

--- src/main/java/recursion/Recursion01.java
+++ src/main/java/recursion/Recursion01.java
@@ -2,8 +2,11 @@ package recursion;
 
 public class Recursion01 {
     public static void print1To100(int n) {
+        if (n > 100) {
+            return;
+        }
         System.out.println(n);
-        print1To100(n);
+        print1To100(n+1);
     }
 
     public static void main(String[] args) {
