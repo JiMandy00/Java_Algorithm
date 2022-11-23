8411c00345d99c17cd3314e3b558e16617a351c2
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-23 10:40:02 +0900

    2022 11 23 - 재귀함수(피보나치 수열 : n번째 까지)

--- src/main/java/recursion/Fibonacci.java
+++ src/main/java/recursion/Fibonacci.java
@@ -16,8 +16,15 @@ public class Fibonacci {
     }
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
+        // n번째
         int input = 5;
         int result = fibonacci.fibonacci(input-1);
         System.out.println(input + "번째 피보나치 수 : " + result);
+
+        // n번째 까지
+        System.out.print(input  + "까지 : ");
+        for (int i = 0; i < input; i++) {
+            System.out.print(fibonacci.fibonacci(i) + " ");
+        }
     }
 }
