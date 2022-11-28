ead64972cd93f2e8e282fa893ed74d72f8f262aa
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-28 09:18:46 +0900

    2022 11 28 - A to Z 모든 경우의 수를 출력하라
    A to Z와 AA to ZZ까지 경우의 수를 모두 출력하는 로직 작성

---
+++ src/main/java/alphabet/Alpabet01.java
@@ -0,0 +1,27 @@
+// 22 11 28
+
+package alphabet;
+
+public class Alpabet01 {
+    public void alphabetTable() {
+        System.out.println("ABCD...");
+        for (int i = 65; i <= 90; i++) {
+            char alphabet = (char)i;
+            System.out.print(alphabet + ", ");
+        }
+        System.out.println("AA, AB, AC...");
+        for (int i = 65; i <= 90; i++) {
+            for (int j = 65; j <= 90 ; j++) {
+                char alphabet01 = (char)i;
+                char alphabet02 = (char)j;
+                System.out.printf("%s%s", alphabet01, alphabet02 + ", ");
+            }
+        }
+
+
+    }
+    public static void main(String[] args) {
+        Alpabet01 alpabet01 = new Alpabet01();
+        alpabet01.alphabetTable();
+    }
+}
